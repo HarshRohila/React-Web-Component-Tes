1a60d608042a6b1083fcf686855f312bc288fc18
Harsh Rohila <rohilaharsh@gmail.com>
2020-05-08 19:42:11 +0530

    added custom elements polyfill

--- src/web-components/web-comp-child/WebCompChildElement.tsx
+++ src/web-components/web-comp-child/WebCompChildElement.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import WebCompChild from './WebCompChild';
+import '@webcomponents/custom-elements';
 
 export default class WebCompChildElement extends HTMLElement {
     config: any;
--- src/web-components/web-comp/WebCompElement.tsx
+++ src/web-components/web-comp/WebCompElement.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import WebComp from './WebComp';
+import '@webcomponents/custom-elements';
 
 export default class WComp extends HTMLElement {
     config: any;
